9667633a99d5cb89446fd5b7e82973954041ccf4
graphoeil <graphoeil@gmail.com>
2022-03-27 19:00:13 +0200

    Version finale

--- js/paralaxe.js
+++ js/paralaxe.js
@@ -6,6 +6,7 @@
 
 	// Top initiaux
 	const topSectionParalaxe = [];
+	const topParalaxes = [];
 	window.addEventListener('DOMContentLoaded', () => {
 		// Reset scroll
 		setTimeout(() => {
@@ -15,6 +16,7 @@
 		setTimeout(() => {
 			paralaxes.forEach((paralaxe) => {
 				topSectionParalaxe.push(paralaxe.parentElement.getBoundingClientRect().top);
+				topParalaxes.push(paralaxe.getBoundingClientRect().top)
 			});
 			window.addEventListener('scroll', checkParalaxe);
 			window.dispatchEvent(new Event('scroll'));
@@ -27,13 +29,29 @@
 		paralaxes.forEach((paralaxe, index) => {
 			// Contenu ?
 			if (paralaxe.classList.contains('contenu')){
-				const topParent = topSectionParalaxe[index];
 				const sectionParent = paralaxe.parentElement;
 				const hauteurParent = sectionParent.getBoundingClientRect().height;
-				if ((topParent - scrollY <= 200) && scrollY <= (topParent + hauteurParent - 100)){
-					const distance = (topParent + hauteurParent) - scrollY;
-					if ((distance/10 < 130) && (distance/10 > 60)){
-						paralaxe.style.transform = `translateY(${ (130 - (distance/10)) }%)`;
+				const topParent = topSectionParalaxe[index];
+				const endParent = topParent + hauteurParent;
+				const hauteurParalaxe = paralaxe.getBoundingClientRect().height;
+				const topParalaxe = topParalaxes[index];
+				const initTop = topParalaxe - topParent;
+				// Nous sommes dans la zone de la section
+				if (scrollY >= topParent && scrollY <= endParent){
+					const distanceParcouru = scrollY - topParent;
+					const pourcentageDeSectionParcouru = (distanceParcouru * 100)/(hauteurParent);
+					const proportionParalaxeParent = (hauteurParalaxe * 100)/hauteurParent;
+					/* le bloc paralaxe ne dépasse pas de la section parent tant que 100% de la section 
+					- la taille en % du paralax - la section parcouru en % est >= 0 */
+					if ((100 - proportionParalaxeParent - pourcentageDeSectionParcouru) >= 0){
+						/* Le déplacement en px corresponds à la hauteur de la section parent - la valeur restante
+						en px du scroll parcouru dans cette section */
+						const deplacementEnPx = hauteurParent - (hauteurParent * (100 - pourcentageDeSectionParcouru))/100;
+						const reste = hauteurParent - hauteurParalaxe - deplacementEnPx;
+						// reste, pour avoir une marge en bas ,-)
+						if (reste > 125){
+							paralaxe.style.top = initTop + deplacementEnPx + 'px';
+						}
 					}
 				}
 				if (scrollY <= 20){ paralaxe.removeAttribute('style'); }
